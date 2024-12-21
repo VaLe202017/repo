fea7f2c9ca7c1041668af0caf7ab56c85fe7ded0
VaLe202017 <leonvalenta1@gmail.com>
2024-12-21 22:20:23 +0100

    Ispravak unos stranice console.error u console.log

--- frontend/quasar-project/src/pages/UnosKnjigaPage.vue
+++ frontend/quasar-project/src/pages/UnosKnjigaPage.vue
@@ -1,75 +1,71 @@
 <template>
   <q-page padding>
     <div class="q-pa-md" style="max-width: 400px">
+      <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+        <q-input
+          filled
+          v-model="naslov"
+          label="Naziv knjige *"
+          lazy-rules
+          :rules="[(val) => (val && val.length > 0) || 'Unesite naziv knjige']"
+        />
 
-<q-form
-  @submit="onSubmit"
-  @reset="onReset"
-  class="q-gutter-md"
->
-  <q-input
-    filled
-    v-model="naslov"
-    label="Naziv knjige *"
-    lazy-rules
-    :rules="[ val => val && val.length > 0 || 'Unesite naziv knjige']"
-  />
-
-  <q-input
-    filled
-    v-model="autor"
-    label="Autor *"
-    lazy-rules
-    :rules="[
-      val => val !== null && val !== '' || 'Unesite naziv autora'
-    ]"
-  />
-  <q-input
-    filled
-    v-model="opis"
-    label="Opis *"
-    lazy-rules
-    :rules="[
-      val => val !== null && val !== '' || 'Unesite opis knjige'
-    ]"
-  />
-  <q-input
-    filled
-    v-model="slika"
-    label="URL slike *"
-    lazy-rules
-    :rules="[
-      val => val !== null && val !== '' || 'Unesite URL za sliku'
-    ]"
-  />
-  <q-input
-    filled
-    type="number"
-    v-model="stanje"
-    label="Stanje knjiga *"
-    
-    lazy-rules
-    :rules="[
-      val => val !== null && val !== '' || 'Unesite stanje knjige'
-    ]"
-  />
-  <div>
-    <q-btn label="Submit" type="submit" color="primary" @click="insertBooks()"/>
-  </div>
-</q-form>
-
-</div>
+        <q-input
+          filled
+          v-model="autor"
+          label="Autor *"
+          lazy-rules
+          :rules="[
+            (val) => (val !== null && val !== '') || 'Unesite naziv autora',
+          ]"
+        />
+        <q-input
+          filled
+          v-model="opis"
+          label="Opis *"
+          lazy-rules
+          :rules="[
+            (val) => (val !== null && val !== '') || 'Unesite opis knjige',
+          ]"
+        />
+        <q-input
+          filled
+          v-model="slika"
+          label="URL slike *"
+          lazy-rules
+          :rules="[
+            (val) => (val !== null && val !== '') || 'Unesite URL za sliku',
+          ]"
+        />
+        <q-input
+          filled
+          type="number"
+          v-model="stanje"
+          label="Stanje knjiga *"
+          lazy-rules
+          :rules="[
+            (val) => (val !== null && val !== '') || 'Unesite stanje knjige',
+          ]"
+        />
+        <div>
+          <q-btn
+            label="Submit"
+            type="submit"
+            color="primary"
+            @click="insertBooks()"
+          />
+        </div>
+      </q-form>
+    </div>
   </q-page>
-
 </template>
 
 <script>
-import { ref } from 'vue';
-import axios from 'axios';
+import { ref } from "vue";
+import axios from "axios";
 
 export default {
-  setup () {
-
+  setup() {
     const naslov = ref(null);
     const autor = ref(null);
     const opis = ref(null);
@@ -81,27 +77,28 @@ export default {
       autor,
       opis,
       slika,
-      stanje
-    }
+      stanje,
+    };
   },
-    methods:{
+  methods: {
     async insertBooks() {
-      const formData={
-        "naslov":this.naslov,
-        "autor":this.autor,
-        "opis":this.opis,
-        "slika":this.slika,
-        "stanje":this.stanje
-      }
-      await axios.post('http://localhost:3000/api/knjige_unos', formData)
-        .then(result => {
-          console.log(result.data.data)
-          console.log(this.books[0].id)
+      const formData = {
+        naslov: this.naslov,
+        autor: this.autor,
+        opis: this.opis,
+        slika: this.slika,
+        stanje: this.stanje,
+      };
+      await axios
+        .post("http://localhost:3000/api/knjige_unos", formData)
+        .then((result) => {
+          console.log(result.data.data);
+          console.log(this.books[0].id);
         })
-        .catch(error => {
-          console.error(error)
-        })
-      },
+        .catch((error) => {
+          console.log(error);
+        });
     },
-  }
+  },
+};
 </script>
